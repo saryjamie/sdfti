98b2501a783c1218fb5e207981b20c22398103de
Sary Jamie <sary.jamie@gmail.com>
2021-07-15 21:13:21 -0700

    adding programs

--- src/components/Programs/Programs.js
+++ src/components/Programs/Programs.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 function Programs() {
-  return <p>List of Programs</p>;
+  return ( <p>List of Programs</p>
+  <p>PPL</p>
+  <p>CPL</p>
+  )
 }
 
 export default Programs;
